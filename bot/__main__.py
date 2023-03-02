76589704641e398227677c47bb2e21339d896d75
MrArkon <mrarkon@outlook.com>
2023-03-02 19:16:19 +0530

    Initial bot structure

---
+++ bot/__main__.py
@@ -0,0 +1,31 @@
+"""MAGPB: MrArkon's General Purpose Bot
+Copyright (C) 2023 MrArkon
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>."""
+from logging import getLogger
+
+import discord
+
+from bot import Bot, config
+
+if __name__ == "__main__":
+    discord.utils.setup_logging()
+    
+    for name, value in config.LOGGING.items():
+        getLogger(name).setLevel(value)
+    
+    discord.VoiceClient.warn_nacl = False
+    
+    Bot().run(config.TOKEN, log_handler=None)
+    
---
+++ bot/config.py
@@ -0,0 +1,24 @@
+"""MAGPB: MrArkon's General Purpose Bot
+Copyright (C) 2023 MrArkon
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>."""
+import tomllib
+
+with open("config.toml", "rb") as f:
+    f = tomllib.load(f)
+
+TOKEN = f["bot"]["token"]
+OWNER_IDS = f["bot"]["owner_ids"]
+
+LOGGING: dict[str, int] = f["logging"]
