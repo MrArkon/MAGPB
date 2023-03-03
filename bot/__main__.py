0d00878d5ecbbda09331d64753af4dc7c377caa1
MrArkon <mrarkon@outlook.com>
2023-03-03 09:10:25 +0530

    Configure jishaku env vars

--- bot/__main__.py
+++ bot/__main__.py
@@ -13,11 +13,15 @@ GNU Affero General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>."""
+import os
 from logging import getLogger
 
 import discord
 
 from bot import Bot, config
+
+os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
+os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True"
 
 if __name__ == "__main__":
     discord.utils.setup_logging()
